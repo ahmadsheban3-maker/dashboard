9b39ff34d7dc8bc6923f06b0c523f6ebd5a5d73e
ahmadsheban3-maker <ahmadsheban3@gmail.com>
2026-04-03 02:38:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -374,7 +374,7 @@ function handleAnimationsToggle() {
 // ==================== INVITE LINK ====================
 function getInviteLink() {
     // Replace with your actual bot client ID
-    const clientId = 'YOUR_BOT_CLIENT_ID';
+    const clientId = '1489146429732818986';
     return `[discord.com](https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot%20applications.commands)`;
 }
 
